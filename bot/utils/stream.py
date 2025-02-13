110457e616b7b3baabcfe686d37c48c649351768
sporestudio <jrodcas@proton.me>
2025-02-13 01:33:22 +0100

    fix: correct path in the urls to recieve
    Change url name with the name of the service in docker compose. May not
    work.

--- bot/utils/stream.py
+++ bot/utils/stream.py
@@ -2,8 +2,8 @@ def get_stream_url(file_path: str, media_type: str) -> str:
     filename = file_path.split("/")[-1]
 
     if media_type == "video":
-        return f"https://localhost:8080/hls/{filename}"
+        return f"http://nginx:8080/hls/{filename}"
     elif media_type == "audio":
-        return f"https://localhost:8080/{filename}"
+        return f"http://icecast:8000/{filename}"
     else:
         raise ValueError("Media type not valid.")
