a9838a9802f42da2ee70b38b5ed38028e378b8b8
sporestudio <jrodcas@proton.me>
2025-02-23 22:03:50 +0100

    refactor: modify url in stream.py
    Modify the base url to redirect to video.html with the streaming.

--- bot/utils/stream.py
+++ bot/utils/stream.py
@@ -2,6 +2,6 @@ import os
 
 def get_stream_url(filename: str, media_type: str) -> str:
     filename = os.path.basename(filename)
-    base_url = "http://localhost/hls/"
+    base_url = "http://localhost/video.html?stream=http://localhost/hls/"
     return f"{base_url}{filename}"
 
