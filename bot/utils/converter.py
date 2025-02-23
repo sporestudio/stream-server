4539a4c1ac30055d5a384a06c6d9a814fe621b5c
sporestudio <jrodcas@proton.me>
2025-02-23 22:06:57 +0100

    refactor: add codec for mp3 in converter
    Add new function to codec to mp3 with ffmpeg.

--- bot/utils/converter.py
+++ bot/utils/converter.py
@@ -1,5 +1,5 @@
 import asyncio
-import ffmpeg
+from ffmpeg.asyncio import FFmpeg
 import os
 
 async def convert_to_hls(input_path: str) -> str:
@@ -7,27 +7,35 @@ async def convert_to_hls(input_path: str) -> str:
     output_path = f"{base_name}.m3u8"
 
     try:
-        process = await asyncio.create_subprocess_exec(
-            "ffmpeg",
-            "-i", input_path,
-            "-c:v", "libx264",
-            "-profile:v", "baseline",
-            "-pix_fmt", "yuv420p",
-            "-preset", "veryfast",
-            "-b:v", "800k",
-            "-c:a", "aac",
-            "-b:a", "128k",
-            "-ar", "44100",
-            "-ac", "2",
-            "-f", "hls",
-            "-hls_time", "10",
-            "-hls_list_size", "0",
-            "-hls_segment_filename", f"{base_name}_%03d.ts",
-            output_path
+        ffmpeg = (
+            FFmpeg()
+            .option("y")
+            .input(input_path)
+            .output(
+                output_path,
+                {"codec:v": "libx264", "filter:v": "scale=1280:-1"},
+                preset="ultrafast",
+                crf=24,
+            )
         )
 
-        await process.wait()
+        await ffmpeg.execute()
         return output_path
-    
+
     except Exception as e:
         raise RuntimeError(f"Error in hls conversion: {str(e)}")
+    
+
+async def convert_to_mp3(input_path: str) -> str:
+    base_name = os.path.splitext(input_path)[0]
+    output_path = f"{base_name}.mp3"
+
+    try:
+        ffmpeg = (
+            FFmpeg()
+        )
+    except Exception as e:
+        raise RuntimeError(f"Error in mp3 conversion: {str(e)}")
+
+    
+    
