efd99c30d3416ca5d6b6219b71792e248f73bc6d
sporestudio <jrodcas@proton.me>
2025-02-24 14:43:49 +0100

    feat: add function to convert mp4 in mp3
    Add function to codec the download video into mp3.

--- bot/utils/converter.py
+++ bot/utils/converter.py
@@ -7,6 +7,29 @@ async def convert_to_hls(input_path: str) -> str:
     output_path = f"{base_name}.m3u8"
 
     try:
+        process = await asyncio.create_subprocess_exec(
+            "ffmpeg",
+            "-i", input_path,
+            "-c:v", "libx264",
+            "-profile:v", "baseline",
+            "-pix_fmt", "yuv420p",
+            "-preset", "veryfast",
+            "-b:v", "800k",
+            "-c:a", "aac",
+            "-b:a", "128k",
+            "-ar", "44100",
+            "-ac", "2",
+            "-f", "hls",
+            "-hls_time", "10",
+            "-hls_list_size", "0",
+            "-hls_segment_filename", f"{base_name}_%03d.ts",
+            output_path
+        )
+
+        await process.wait()
+        return output_path
+
+        '''
         ffmpeg = (
             FFmpeg()
             .option("y")
@@ -21,6 +44,7 @@ async def convert_to_hls(input_path: str) -> str:
 
         await ffmpeg.execute()
         return output_path
+        '''
 
     except Exception as e:
         raise RuntimeError(f"Error in hls conversion: {str(e)}")
@@ -33,7 +57,21 @@ async def convert_to_mp3(input_path: str) -> str:
     try:
         ffmpeg = (
             FFmpeg()
+            .option("y")
+            .input(input_path)
+            .output(
+                output_path,
+                format="mp3",          
+                acodec="libmp3lame",     
+                **{"b:a": "128k",      
+                   "ar": "44100",      
+                   "ac": "2"} 
+            )
         )
+
+        await ffmpeg.execute()
+        return output_path
+
     except Exception as e:
         raise RuntimeError(f"Error in mp3 conversion: {str(e)}")
 
