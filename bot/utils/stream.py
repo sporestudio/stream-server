cb69c1ff709c12994cf1cd6fb020cd9d2f11d07d
sporestudio <jrodcas@proton.me>
2025-03-02 22:24:53 +0100

    fix: correct url in stream.py
    Fix output url of get stream url function.

--- bot/utils/stream.py
+++ bot/utils/stream.py
@@ -4,8 +4,8 @@ def get_stream_url(filename: str, media_type: str) -> str:
     filename = os.path.basename(filename)
 
     if media_type == "video":
-        base_url = "http://localhost/video.html?stream=http://localhost/hls/"
+        base_url = "http://localhost/video/video.html?stream=http://localhost/hls/"
         return f"{base_url}{filename}"
     else:
-        base_url = "http://localhost/audio/"
-    return f"{base_url}{filename}"
+        base_url = "http://localhost/audio"
+    return f"{base_url}"
