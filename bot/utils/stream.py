ba2453759ba04c5b53bdc2f695bbfbe6d97c7577
sporestudio <jrodcas@proton.me>
2025-03-02 03:38:39 +0100

    feat: add convert to ogg function
    Add function to convert to .ogg format the audio downloaded from
    YouTube.

--- bot/utils/stream.py
+++ bot/utils/stream.py
@@ -2,6 +2,10 @@ import os
 
 def get_stream_url(filename: str, media_type: str) -> str:
     filename = os.path.basename(filename)
-    base_url = "http://localhost/video.html?stream=http://localhost/hls/"
-    return f"{base_url}{filename}"
 
+    if media_type == "video":
+        base_url = "http://localhost/video.html?stream=http://localhost/hls/"
+        return f"{base_url}{filename}"
+    else:
+        base_url = "http://localhost/audio/"
+    return f"{base_url}{filename}"
