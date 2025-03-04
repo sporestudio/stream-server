94d1c251b1ab37b15d9f2ca660a59bce23f5363f
sporestudio <jrodcas@proton.me>
2025-03-04 22:09:17 +0100

    feat: Add infraestructure for dynamic DNS
    Add Dockerfile with the proper configuration for make a request to
    IONOS every minute and have the IP updated all time.

---
+++ dyndns/geturl.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+API_KEY=$(cat user.key)
+output=$(curl -X "POST" "https://api.hosting.ionos.com/dns/v1/dyndns" \
+    -H "accept: application/json" \
+    -H "X-API-Key: ${API_KEY}" \
+    -H "Content-Type: application/json" \
+    -d '{
+        "domains": [
+            "stream.sporestudio.me"
+        ],
+        "description": "Dynamic DNS"
+    }') 
+
+# process the curl and getting the updateUrl using jq
+updateUrl=$(echo "$output" | jq -r '.updateUrl')
+
+# save the url into a file and output its content
+echo $updateUrl > update_url
+cat update_url
