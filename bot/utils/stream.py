7903f9bb75b34ae8e01b01569c38a87c51f0f6be
sporestudio <jrodcas@proton.me>
2025-03-05 14:29:16 +0100

    feat: modify video url
    Modify video output url to pass an url for VLC and another for web.

--- bot/utils/stream.py
+++ bot/utils/stream.py
@@ -4,8 +4,9 @@ def get_stream_url(filename: str, media_type: str) -> str:
     filename = os.path.basename(filename)
 
     if media_type == "video":
-        base_url = "https://stream.sporestudio.me/video/"
-        return f"{base_url}{filename}"
+        vlc_url = "https://stream.sporestudio.me/hls/"
+        web_url = "https://stream.sporestudio.me/video/"
+        return f"VLC url: {vlc_url}{filename}\n\nWeb url: {web_url}"
     else:
         base_url = "https://stream.sporestudio.me/audio"
     return f"{base_url}"
