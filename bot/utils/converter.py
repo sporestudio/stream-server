ce1efc2bf40f5256f27fbc71841b9ad56780f4a0
sporestudio <jrodcas@proton.me>
2025-03-05 14:35:02 +0100

    chore: changes in converter.py
    Make tests with the ffmpeg directives to improve performance.

--- bot/utils/converter.py
+++ bot/utils/converter.py
@@ -44,21 +44,31 @@ async def convert_to_hls(input_path: str) -> str:
         process = await asyncio.create_subprocess_exec(
             "ffmpeg",
             "-i", input_path,
-            "-c:v", "libx264",
-            "-profile:v", "main",
-            "-pix_fmt", "yuv420p",
-            "-preset", "veryfast",
-            "-b:v", "800k",
-            "-c:a", "aac",
-            "-b:a", "128k",
-            "-ar", "44100",
-            "-ac", "2",
-            "-f", "hls",
-            "-hls_time", "10",
+            "-c:v", "libx264",         
+            "-preset", "veryfast",   
+            "-b:v", "800k",            
+            "-c:a", "aac",            
+            "-ar", "44100",            
+            "-ac", "1",                
+            "-f", "hls",              
+            "-hls_time", "10",         
             "-hls_list_size", "0",
-            "-hls_segment_filename", f"{base_name}_%03d.ts",
             output_path
         )
+
+        '''
+            "ffmpeg",
+            "-i", output_file,        
+            "-c:v", "libx264",         
+            "-preset", "veryfast",   
+            "-b:v", "800k",            
+            "-c:a", "aac",            
+            "-ar", "44100",            
+            "-ac", "1",                
+            "-f", "hls",              
+            "-hls_time", "10",         
+            "-hls_list_size", "0",
+        '''
 
         await process.wait()
         return output_path
@@ -91,7 +101,7 @@ async def convert_to_ogg(input_path: str) -> str:
         ffmpeg_command = await asyncio.create_subprocess_exec(
             "ffmpeg", "-y",
             "-i", input_path,
-            "-vn",                # Sin video
+            "-vn",               
             "-acodec", "libvorbis",
             "-b:a", "128k",
             output_path
