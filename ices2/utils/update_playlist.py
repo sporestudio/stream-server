d1a99fcf640b9347f940a5eb76bd48e2c1f2c557
sporestudio <jrodcas@proton.me>
2025-03-05 21:01:17 +0100

    chore: add songs in a dynamic way to playlist with watchdog
    Add new functionality to add the new songs in the server in a dynamic way using watchdog. This script will be monitoring the shared folder waiting for new additions.

--- ices2/utils/update_playlist.py
+++ ices2/utils/update_playlist.py
@@ -1,15 +1,38 @@
 import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
 
-def update_playlist(shared: str, playlist_file: str):
-    with open(playlist_file, 'w') as playlist:
-        for root, _, files in os.walk(shared_dir):
-            for file in files:
-                if file.endswith('.ogg'):
-                    file_path = os.path.join(root, file)
-                    playlist.write(f"{file_path}\n")
+class AudioFileHandler(FileSystemEventHandler):
+    def __init__(self, shared_dir, playlist_file):
+        self.shared_dir = shared_dir
+        self.playlist_file = playlist_file
+        self.update_playlist()
 
+    def on_created(self, event):
+        if event.is_directory:
+            return
+        if event.src_path.endswith('.ogg'):
+            self.update_playlist()
+
+    def update_playlist(self):
+        with open(self.playlist_file, 'w') as playlist:
+            for root, _, files in os.walk(self.shared_dir):
+                for file in files:
+                    if file.endswith('.ogg'):
+                        file_path = os.path.join(root, file)
+                        playlist.write(f"{file_path}\n")
 
 if __name__ == "__main__":
     shared_dir = "/shared"
     playlist_file = "/var/lib/ices2/playlist"
-    update_playlist(shared_dir, playlist_file)
+    event_handler = AudioFileHandler(shared_dir, playlist_file)
+    observer = Observer()
+    observer.schedule(event_handler, path=shared_dir, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
