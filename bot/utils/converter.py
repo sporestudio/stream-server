6a10266947a8f4e5df16929f84c5be9ec42945db
sporestudio <jrodcas@proton.me>
2025-03-05 12:17:49 +0100

    chore: try differents codec solutions
    Try to perform the codec. Still not work.

--- bot/utils/converter.py
+++ bot/utils/converter.py
@@ -2,6 +2,40 @@ import asyncio
 from ffmpeg.asyncio import FFmpeg
 import os
 
+'''
+async def convert_to_hls(input_path: str) -> str:
+    base_name = os.path.splitext(input_path)[0]
+    # Asegúrate de que el directorio de salida exista:
+    output_dir = os.path.dirname(base_name)
+    if output_dir and not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+    
+    # Definir el path del manifest (output_path)
+    output_path = f"{base_name}.m3u8"
+    
+    process = await asyncio.create_subprocess_exec(
+        "ffmpeg",
+        "-i", input_path,
+        "-c:v", "copy",
+        "-start_number", "0",
+        "-f", "hls",
+        "-hls_time", "10",
+        "-hls_list_size", "0",
+        "-hls_segment_filename", f"{base_name}_%03d.ts",
+        output_path,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    
+    stdout, stderr = await process.communicate()
+    if process.returncode != 0:
+        error_msg = stderr.decode()
+        raise Exception(f"ffmpeg failed with error: {error_msg}")
+    
+    return output_path
+'''
+
+
 async def convert_to_hls(input_path: str) -> str:
     base_name = os.path.splitext(input_path)[0] 
     output_path = f"{base_name}.m3u8"
@@ -11,7 +45,7 @@ async def convert_to_hls(input_path: str) -> str:
             "ffmpeg",
             "-i", input_path,
             "-c:v", "libx264",
-            "-profile:v", "baseline",
+            "-profile:v", "main",
             "-pix_fmt", "yuv420p",
             "-preset", "veryfast",
             "-b:v", "800k",
@@ -28,8 +62,11 @@ async def convert_to_hls(input_path: str) -> str:
 
         await process.wait()
         return output_path
+    
+    except Exception as e:
+        raise RuntimeError(f"Error in hls conversion: {str(e)}")
 
-        '''
+    '''
         ffmpeg = (
             FFmpeg()
             .option("y")
@@ -44,11 +81,8 @@ async def convert_to_hls(input_path: str) -> str:
 
         await ffmpeg.execute()
         return output_path
-        '''
-
-    except Exception as e:
-        raise RuntimeError(f"Error in hls conversion: {str(e)}")
-    
+    '''
+  
 
 async def convert_to_ogg(input_path: str) -> str:
     output_path = input_path.rsplit(".", 1)[0] + ".ogg"
