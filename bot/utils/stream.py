dcf7af126b1cb980544f0b473ab68e33fe0399a9
sporestudio <jrodcas@proton.me>
2025-03-05 12:16:58 +0100

    refactor: correct output url
    Correct final output url.

--- bot/utils/stream.py
+++ bot/utils/stream.py
@@ -4,7 +4,7 @@ def get_stream_url(filename: str, media_type: str) -> str:
     filename = os.path.basename(filename)
 
     if media_type == "video":
-        base_url = "https://stream.sporestudio.me/video/index.html?stream=http://localhost/hls/"
+        base_url = "https://stream.sporestudio.me/video/"
         return f"{base_url}{filename}"
     else:
         base_url = "https://stream.sporestudio.me/audio"
